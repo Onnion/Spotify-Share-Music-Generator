454d91f9a7836c33694a545ea614a122a143f316
Marcio <marcio.hsdl@gmail.com>
2025-07-06 02:09:06 -0300

    fix: fix terraform apply

--- packages/spotify-nest/deploy/iac/backend.tf
+++ packages/spotify-nest/deploy/iac/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "spotify-share-music-generator.tfstate"
-    key     = "spotify-share-music-generator.tfstate/terraform.tfstate"
-    region  = "sa-east-1"
-    encrypt = true
+    bucket = "spotify-share-music-generator.tfstate"
+    region = "sa-east-1"
   }
 }
