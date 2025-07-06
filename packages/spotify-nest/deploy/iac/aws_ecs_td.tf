1ead1a50a54ed9552e1f52eea4c2b5cbaf04081a
Marcio <marcio.hsdl@gmail.com>
2025-07-06 06:22:18 -0300

    chore: remove unused policy

--- packages/spotify-nest/deploy/iac/aws_ecs_td.tf
+++ packages/spotify-nest/deploy/iac/aws_ecs_td.tf
@@ -1,23 +1,3 @@
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "${var.ecr_repository_name}-ecs-execution"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 resource "aws_ecs_task_definition" "spotify_nestjs_td" {
   family                   = "${var.ecr_repository_name}-task"
   network_mode             = "awsvpc"
