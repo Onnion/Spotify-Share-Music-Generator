826c337561d2fa62b6d3f4d2ab502efd1a47df3e
Marcio <marcio.hsdl@gmail.com>
2025-07-06 14:23:04 -0300

    fix: remove 0.0.0.0 from vpc blocked cdir

--- packages/spotify-nest/deploy/iac/aws_ecs_vpc.tf
+++ packages/spotify-nest/deploy/iac/aws_ecs_vpc.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "spotify_nestjs" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [module.vpc.vpc_cidr_block]
   }
 
   egress {
