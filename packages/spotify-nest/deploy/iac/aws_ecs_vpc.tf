2026b5a5eef730fa2885dff2e7f9310aa475eb10
Marcio <marcio.hsdl@gmail.com>
2025-07-06 14:13:47 -0300

    feat: update vpc configs

--- packages/spotify-nest/deploy/iac/aws_ecs_vpc.tf
+++ packages/spotify-nest/deploy/iac/aws_ecs_vpc.tf
@@ -21,8 +21,8 @@ resource "aws_security_group" "spotify_nestjs" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = var.container_port
-    to_port     = var.container_port
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
