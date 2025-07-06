f2f87b337bca498bf5bb0f2e16ba6a2e2647ec90
Marcio <marcio.hsdl@gmail.com>
2025-07-06 06:04:46 -0300

    fix: add cpu config to task definition

--- packages/spotify-nest/deploy/iac/aws_ecs_td.tf
+++ packages/spotify-nest/deploy/iac/aws_ecs_td.tf
@@ -23,6 +23,7 @@ resource "aws_ecs_task_definition" "spotify_nestjs_td" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = "256"
   memory                   = "512"
   container_definitions    = file("${path.module}/task_definition/${var.environment}.json")
 }
