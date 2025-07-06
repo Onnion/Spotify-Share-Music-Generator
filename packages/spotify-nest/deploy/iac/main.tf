ede2f6e8562e58736afc5daded02e4d3ddcd1cb5
Marcio <marcio.hsdl@gmail.com>
2025-07-06 01:19:02 -0300

    chore: remove create iam user from iac

--- packages/spotify-nest/deploy/iac/main.tf
+++ packages/spotify-nest/deploy/iac/main.tf
@@ -5,36 +5,3 @@ provider "aws" {
 resource "aws_ecr_repository" "nestjs" {
   name = var.ecr_repository_name
 }
-
-resource "aws_iam_user" "github_actions" {
-  name = "github-actions"
-}
-
-resource "aws_iam_user_policy" "ecr_policy" {
-  name = "GitHubActionsECRPolicy"
-  user = aws_iam_user.github_actions.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:PutImage",
-          "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload",
-        ],
-        Effect   = "Allow",
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_access_key" "github_actions_key" {
-  user = aws_iam_user.github_actions.name
-}
